f4e07cabd3ad0c4bc87f912ab0205a9156a01cf0
Abhinav2023 <abhinavbansal231101@gmail.com>
2022-01-14 12:15:23 +0530

    Revisited All Changes

--- Foundations/JS/index.js
+++ Foundations/JS/index.js
@@ -1,94 +1,108 @@
+let playerSelection, playerWinCount = 0, computerWinCount = 0;;
 function computerPlay(){
-        let number=Math.floor((Math.random()*3)+1);
-        if(number===1) return "rock";
-        else if(number==2) return "scissor";
-        else return "paper";
+        let number = Math.floor ( ( Math.random() * 3) + 1);
+        switch (number) {
+                case 1:
+                        return "rock";
+                        break;
+                case 2: 
+                        return "scissor";
+                        break;
+                case 3: 
+                        return "paper";
+        }
 }
 
-let playerWinCount=0,computerWinCount=0;
-function playRound(playerSelection,computerSelection){
-        if(playerSelection===computerSelection) return "Draw";
-        if(playerSelection==="rock"){
-                if(computerSelection==='paper'){
-                        computerWinCount++;
-                        return "Computer Win";
+function playRound ( playerSelection , computerSelection ) {
+        if ( playerSelection === computerSelection ) return "Draw";
+        if ( playerSelection === "rock" ) {
+                if ( computerSelection === 'paper' ) {
+                        computerWinCount++ ;
+                        return "Computer Win" ;
                 } 
-                else{
-                        playerWinCount++;
-                        return "Player Win";
+                playerWinCount++ ;
+                return "Player Win" ;
+        }else if ( playerSelection === 'scissor' ) {
+                if ( computerSelection === "paper" ) {
+                        playerWinCount++ ;
+                        return "Player Win" ;
                 }
-        }else if(playerSelection==='scissor'){
-                if(computerSelection==="paper"){
-                        playerWinCount++;
-                        return "Player Win";
-                }
-                else{
-                        computerWinCount++;
-                        return "Computer Win";
-                }
+                computerWinCount++ ;
+                return "Computer Win" ;
         }else {
-                if(computerSelection==="rock"){
-                        playerWinCount++;
-                        return "Player Win";
+                if ( computerSelection === "rock" ) {
+                        playerWinCount++ ;
+                        return "Player Win" ;
                 }
-                else{
-                        computerWinCount++;
-                        return "Computer Win";
-                }
+                computerWinCount++ ;
+                return "Computer Win" ;
         }
 }
 
-function showResults(){
-        const res=document.querySelector('#result');
-        if(playerWinCount===5){
-                res.textContent="Finally Player Win";
-        }else{
-                res.textContent="Finally Computer Win"
+function showResults () {
+        const res = document.querySelector( '#result' );
+        if ( playerWinCount === 5 ) {
+                res.textContent = "Finally Player Win" ;
+        } else {
+                res.textContent = "Finally Computer Win" ;
         }
-        playerWinCount=0,computerWinCount=0;
+        playerWinCount = 0, computerWinCount = 0 ;
 }
 
 
-let playerSelection;
-function makeMoveAndShowResult(){
-        let computerSelection=computerPlay();
+
+function makeMoveAndShowResult () {
+        const playercnt = document.querySelector( '#playerCount' ) ;
+        const computercnt = document.querySelector( '#computerCount' ) ;
+        let computerSelection = computerPlay() ;
+        
         console.log(computerSelection);
-        playRound(playerSelection,computerSelection);
-        const playercnt=document.querySelector('#playerCount');
-        playercnt.textContent="Player - "+playerWinCount;
-        const computercnt=document.querySelector('#computerCount');
-        computercnt.textContent="Computer - "+computerWinCount;
+
+        playRound ( playerSelection , computerSelection ) ;
+        playercnt.textContent = "Player - " + playerWinCount ;
+        computercnt.textContent = "Computer - " + computerWinCount ;
 }
+
+function paperButtonOnClickHandler () {
+        const btnPaper = document.querySelector( '#paper' );
+        btnPaper.addEventListener ( 'click' ,() => {
+                playerSelection = "paper";
+                makeMoveAndShowResult() ;
+                if ( computerWinCount === 5 || playerWinCount === 5) {
+                        showResults();
+                }
+        })
+} 
+
+function rockButtonOnClickHandler () {
+        const btnRock = document.querySelector( '#rock' );
+        btnRock.addEventListener( 'click' ,() => {
+                playerSelection = "rock" ;
+                makeMoveAndShowResult() ;
+                if ( computerWinCount === 5 || playerWinCount === 5 ) {
+                        showResults();
+                }
+        })
+}
+function scissorButtonOnClickHandler () {
+        const btnScissor = document.querySelector( '#scissor' ) ;
+        btnScissor.addEventListener( 'click' , () => {
+                playerSelection = "scissor" ;
+                makeMoveAndShowResult() ;
+                if ( computerWinCount === 5 || playerWinCount === 5 ) {
+                        showResults();
+                }
+        })
+}
+
 
 // If Paper is selected
-const btnPaper=document.querySelector('#paper');
-btnPaper.onclick=()=>{
-        playerSelection="paper";
-        makeMoveAndShowResult();
-        if(computerWinCount===5 || playerWinCount===5){
-                showResults();
-        }
-}
+paperButtonOnClickHandler();
 
 // If rock is selected
-const btnRock=document.querySelector('#rock');
-btnRock.onclick=()=>{
-        playerSelection="rock";
-        makeMoveAndShowResult();
-        if(computerWinCount===5 || playerWinCount===5){
-                showResults();
-        }
-}
+rockButtonOnClickHandler();
 
 // If scissor is selected
-const btnScissor=document.querySelector('#scissor');
-btnScissor.onclick=()=>{
-        playerSelection="scissor";
-        makeMoveAndShowResult();
-        if(computerWinCount===5 || playerWinCount===5){
-                showResults();
-        }
-}
-
+scissorButtonOnClickHandler();
 
 
