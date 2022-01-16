fc776072dfeb71d8312f271b21d268b78537b42d
Abhinav2023 <abhinavbansal231101@gmail.com>
2022-01-16 19:02:00 +0530

    Added Grid

--- Foundations/JS/Rock Paper/Etch-a-sketch/index.js
+++ Foundations/JS/Rock Paper/Etch-a-sketch/index.js
@@ -4,10 +4,13 @@ let column=prompt("Please enter number of columns",16);
 console.log(row);
 console.log(column);
 var div=document.createElement("div");
+div.addEventListener("mouseover",function(){
+        console.log("Change Background");
+        div.classList.add('hoverEffect');
+},false);
 div.classList.add("mystyle");
-// div.style.height="1px";
-// div.style.width="1px";
-div.style.border="2px solid black";
+div.style.border="1px solid black";
+
 var rowDiv=document.createElement("div");
 rowDiv.classList.add("row");
 for(let i=0;i<column;i++){
